590afbddf628433b27e8407d38a4940c96a65557
Riya Agrawal <riya@scrollstack.com>
2021-09-08 16:11:30 +0530

    drop

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
-  <div class="drop-zone">
+  <div
+    class="drop-zone"
+    @drop="onDrop($event, 1)"
+    @dragenter.prevent
+    @dragover.prevent
+  >
     <div
       v-for="item in getList(1)"
       :key="item.id"
       class="drag-el"
       draggable="true"
+      @dragstart="startDrag($event, item)"
     >
       {{ item.title }}
     </div>
   </div>
 
-  <div class="drop-zone">
+  <div
+    class="drop-zone"
+    @drop="onDrop($event, 2)"
+    @dragenter.prevent
+    @dragover.prevent
+  >
     <div
       v-for="item in getList(2)"
       :key="item.id"
@@ -24,32 +35,39 @@
 </template>
 
 <script>
-import {ref} from 'vue'
-export default{
-  setup(){
+import { ref } from 'vue';
+export default {
+  setup() {
     const items = ref([
-      {id:0, title: 'item A', list: 1},
-      {id:1, title: 'item B', list: 1},
-      {id:2, title: 'item C', list: 2},
-    ])
+      { id: 0, title: 'item A', list: 1 },
+      { id: 1, title: 'item B', list: 1 },
+      { id: 2, title: 'item C', list: 2 },
+    ]);
 
     const getList = (list) => {
-      return items.value.filter((item) => item.list == list)
-    }
+      return items.value.filter((item) => item.list == list);
+    };
 
-    const startDrag = {event, item} => {
-      console.log(item)
-      event.dataTransfer.dropEffect = 'move'
-      event.dataTransfer.effectAllowed = 'move'
-      event.dataTransfer.setData('itemID', item.id)
-    }
+    const startDrag = (event, item) => {
+      console.log(item);
+      event.dataTransfer.dropEffect = 'move';
+      event.dataTransfer.effectAllowed = 'move';
+      event.dataTransfer.setData('itemID', item.id);
+    };
 
-    return{
+    const onDrop = (event, list) => {
+      const itemID = event.dataTransfer.getData('itemID');
+      const item = items.value.find((item) => item.id == itemID);
+      item.list = list;
+    };
+
+    return {
       getList,
       startDrag,
-    }
-  }
-}
+      onDrop,
+    };
+  },
+};
 </script>
 
 <style>
